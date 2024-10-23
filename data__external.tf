ab21ff621a2bbb6a28c75795817341ebde4d120c
tannevaled <tannevaled@users.noreply.github.com>
2024-10-23 14:27:50 +0200

    wip

--- data__external.tf
+++ data__external.tf
@@ -3,7 +3,7 @@ data external get-volumes {
     # https://registry.terraform.io/providers/hashicorp/external/latest/docs/data-sources/external
     #
     program = [
-        "/usr/bin/env", "-S", "pkgx", "+openstack^7.2", "+jq^1.7", "+bash^5", "bash",
+        "/usr/bin/env", "-S", "pkgx", "+jq^1.7", "+bash^5", "bash",
         "${path.module}/scripts/get-volumes.pkgx"
     ]
 
