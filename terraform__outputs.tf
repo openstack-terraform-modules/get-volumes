55ecadf8a35c507bf553ec64005af0363a0f7233
tannevaled <tannevaled@users.noreply.github.com>
2024-10-23 13:55:36 +0200

    wip

--- terraform__outputs.tf
+++ terraform__outputs.tf
@@ -2,11 +2,5 @@ output outputs {
     #value = {
     #    for k, v in data.external.get-volumes : k => v
     #}
-    value = {
-        volumes = lookup(
-            data.external.get-volumes.result,
-            "volumes",
-            "NOT FOUND ERROR"
-        )
-    }
+    value = data.external.get-volumes.result
 }
