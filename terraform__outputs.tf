d4a92045b4b347d8cc06083ad028f803c1aa1b59
tannevaled <tannevaled@users.noreply.github.com>
2024-10-23 16:02:24 +0200

    wip

--- terraform__outputs.tf
+++ terraform__outputs.tf
@@ -1,9 +1,7 @@
 output inventory_file_path {
-    value = {
-        inventory_file_path = lookup(
-            data.external.get-volumes.result,
-            "inventory_file_path",
-            "NOT FOUND ERROR"
-        )
-    }
+    value = lookup(
+        data.external.get-volumes.result,
+        "inventory_file_path",
+        "NOT FOUND ERROR"
+    )
 }
